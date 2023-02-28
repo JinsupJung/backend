93190964198b13be22d01380eb9456165a48ec85
JinsupJung <cool4uh@gmail.com>
2023-02-28 10:46:14 +0900

    frontend session storage 전까지 1:27:16

--- src/main/java/com/example/backend/controller/MemberController.java
+++ src/main/java/com/example/backend/controller/MemberController.java
@@ -4,6 +4,7 @@ import com.example.backend.entity.Member;
 import com.example.backend.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,7 @@ public class MemberController {
     MemberRepository memberRepository;
 
     @PostMapping("/api/account/login")
-    public int login(@RequestParam Map<String, String> params) {
+    public int login(@RequestBody Map<String, String> params) {
         Member member = memberRepository.findByEmailAndPassword(params.get("email"), params.get("password"));
 
         if (member != null) {
--- src/main/java/com/example/backend/repository/MemberRepository.java
+++ src/main/java/com/example/backend/repository/MemberRepository.java
@@ -4,5 +4,5 @@ import com.example.backend.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Integer> {
-    Member findByEmailAndPassword(String email, String password)
+    Member findByEmailAndPassword(String email, String password);
 }
